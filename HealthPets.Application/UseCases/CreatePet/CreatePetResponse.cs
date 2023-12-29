4185f22d6880a5f981a2ce5b70d85328143a6c65
wellington chida <wellingtonchida@protonmail.com>
2023-12-28 23:14:32 -0300

    ✨feat: change data base from sqlserver,add `PetsController` and `ValidationBehavior`

--- HealthPets.Application/UseCases/CreatePet/CreatePetResponse.cs
+++ HealthPets.Application/UseCases/CreatePet/CreatePetResponse.cs
@@ -2,7 +2,7 @@
 {
     public sealed record CreatePetResponse
     {
-        public Ulid Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Owner { get; set; }
     }
---
+++ Healthpets.Api/Controllers/PetsController.cs
@@ -0,0 +1,26 @@
+﻿using HealthPets.Application.UseCases.CreatePet;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthPets.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PetsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CreatePetResponse>> Create(CreatePetRequest req, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(req, cancellationToken);
+            return Ok(response);
+        }
+    }
+}
